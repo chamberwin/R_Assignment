8bf88c81b3579078a578005eb20eef2f1481ded8
Heather Chamberlain <hchamber@iastate.edu>
2022-03-10 15:36:59 -0600

    After running code for errors, final upload. Biggest change to GC visuals to include G/G, C/C, G/C, C/G for certainty.

--- R_Assignment_Script.R
+++ R_Assignment_Script.R
@@ -1,116 +1,99 @@
-library(tidyverse)
+## Install/load packages
+library(tidyverse)  #For data maniupluation
 library(dplyr)
 install.packages("janitor")
 install.packages("data.table")
 library(janitor)
 library(data.table)
-geno <- read.delim("https://raw.githubusercontent.com/chamberwin/BCB546-Spring2022/main/assignments/UNIX_Assignment/fang_et_al_genotypes.txt", header = TRUE, sep = "\t")
+
+## Upload raw data
+geno <- read.delim("https://raw.githubusercontent.com/chamberwin/BCB546-Spring2022/main/assignments/UNIX_Assignment/fang_et_al_genotypes.txt", header = TRUE, sep = "\t") #Download raw files from class repository for genotypes and SNPs
 snp_pos <- read.delim("https://raw.githubusercontent.com/chamberwin/BCB546-Spring2022/main/assignments/UNIX_Assignment/snp_position.txt", header = TRUE, sep = "\t") 
-ncol(geno)
-dim(geno)
-length(geno)
-object.size(geno)
-summary(geno)
-nrow(snp_pos)
-ncol(snp_pos)
-dim(snp_pos)
-length(snp_pos)
+
+## Inspect data
+ncol(geno) #Number of columns for genotype data is 986
+nrow(geno) #Number of rows is 2781
+dim(geno) #Dimensions of rows (2781) and columns (986)
+length(geno) #Number of columns is 986
+object.size(geno) #Size of file is 22681376 bytes
+summary(geno) #Because of large number of columns, not super information
+head(geno) #A little better than summary at seeing structure
+ncol(snp_pos) #Number of columns in SNP position data is 15
+nrow(snp_pos) #Number of rows in SNP position data is 983
+dim(snp_pos) #Dimensions of rows (983) and columns (15)
+length(snp_pos) #Number of columns is 15
 object.size(snp_pos)
-summary(snp_pos)
-snp_cut <- snp_pos %>%
-  select(SNP_ID, Chromosome, Position) %>%
-  arrange(SNP_ID)
-view(snp_cut)
+summary(snp_pos) #Size of file is 327392 bytes
+head(snp_pos) #Look at data structure
+
+## Prepare SNP position file for join
+snp_cut <- snp_pos %>% #Create prepared file for join
+  select(SNP_ID, Chromosome, Position) %>% #Select only these columns
+  arrange(SNP_ID) #Arrange by SNP_ID
+view(snp_cut) #Check work
+
 ## Maize Join
 geno_maize <- geno %>%
-  filter(Group %in% c("ZMMIL", "ZMMLR", "ZMMMR", "Sample_ID")) %>%
-  # filter by maize groups only, including sample ID
-  t() %>%
-  # transpose - note it's added automatic column names (V1, V2, etc. in new header)
-  as.data.frame() %>%
-  # make sure it's a dataframe
-  setDT(keep.rownames = TRUE) %>%
-  #gives column 1 a placeholder name as well
-  row_to_names(row_number = 1) %>%
-  # sets first row as column names
+  filter(Group %in% c("ZMMIL", "ZMMLR", "ZMMMR", "Sample_ID")) %>% # filter by maize groups only, including sample ID
+  t() %>% # transpose - note it's added automatic column names (V1, V2, etc. in new header)
+  as.data.frame() %>% # make sure it's a dataframe
+  setDT(keep.rownames = TRUE) %>% #gives column 1 a placeholder name as well
+  row_to_names(row_number = 1) %>% # sets first row as column names
   rename(SNP_ID = Sample_ID) # rename first column as SNP_ID so we can join on like column names
 view(geno_maize) #double check joined by SNP_ID
 join_maize <- snp_cut %>%
   inner_join(geno_maize, by = "SNP_ID") # joins both files - inner_join makes sure we take only SNP_IDs in both files
 view(join_maize) #double check it joined correctly
-##Teosinte Join
+
+## Teosinte Join
 geno_teosinte <- geno %>%
-  filter(Group %in% c("ZMPBA", "ZMPIL", "ZMPJA")) %>%
-  # filter by maize groups only, including sample ID
-  t() %>%
-  # transpose - note it's added automatic column names (V1, V2, etc. in new header)
-  as.data.frame() %>%
-  # make sure it's a dataframe
-  setDT(keep.rownames = TRUE) %>%
-  #gives column 1 a placeholder name as well
-  row_to_names(row_number = 1) %>%
-  # sets first row as column names
-  rename(SNP_ID = Sample_ID)
-# rename first column as SNP_ID so we can join on like column names
+  filter(Group %in% c("ZMPBA", "ZMPIL", "ZMPJA")) %>% # filter by maize groups only, including sample ID
+  t() %>% # transpose - note it's added automatic column names (V1, V2, etc. in new header)
+  as.data.frame() %>% # make sure it's a dataframe
+  setDT(keep.rownames = TRUE) %>% #gives column 1 a placeholder name as well
+  row_to_names(row_number = 1) %>% # sets first row as column names
+  rename(SNP_ID = Sample_ID) # rename first column as SNP_ID so we can join on like column names
 view(geno_teosinte) #double check it is orded by SNP_ID
 join_teosinte <- snp_cut %>%
-  inner_join(geno_teosinte, by = "SNP_ID")
-# joins both files - inner_join makes sure we take only SNP_IDs in both files
+  inner_join(geno_teosinte, by = "SNP_ID") # joins both files - inner_join makes sure we take only SNP_IDs in both files
 view(join_teosinte) #double check it joined correctly
+
+## Sort Joined Maize
 sort_maize <- join_maize %>%
-  arrange(as.numeric(Chromosome), as.numeric(Position))
-# sorts by chromosome number and position, in ascending order
-view(sort_maize)
-chr1_maize <- sort_maize %>%
-  filter(Chromosome == 1)
-view(chr1_maize)
-chr2_maize <- sort_maize %>%
+  arrange(as.numeric(Chromosome), as.numeric(Position)) # sorts by chromosome number and position, in ascending order
+view(sort_maize) #Check work
+chr1_maize <- sort_maize %>% #Creates object for chr1
+  filter(Chromosome == 1) #filters for chromosome 1 only
+view(chr1_maize) #Check work
+chr2_maize <- sort_maize %>% #Repeat for all chromosomes
   filter(Chromosome == 2)
+view(chr2_maize)
 chr3_maize <- sort_maize %>%
   filter(Chromosome == 3)
+view(chr3_maize)
 chr4_maize <- sort_maize %>%
   filter(Chromosome == 4)
+view(chr4_maize)
 chr5_maize <- sort_maize %>%
   filter(Chromosome == 5)
+view(chr5_maize)
 chr6_maize <- sort_maize %>%
   filter(Chromosome == 6)
+view(chr6_maize)
 chr7_maize <- sort_maize %>%
   filter(Chromosome == 7)
+view(chr7_maize)
 chr8_maize <- sort_maize %>%
   filter(Chromosome == 8)
+view(chr8_maize)
 chr9_maize <- sort_maize %>%
   filter(Chromosome == 9)
+view(chr9_maize)
 chr10_maize <- sort_maize %>%
   filter(Chromosome == 10)
 view(chr10_maize)
-sort_teosinte <- join_teosinte %>%
-  arrange(as.numeric(Chromosome), as.numeric(Position)) #arrange teosinte object by chromosome and position
-view(sort_teosinte)
-chr1_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 1) #create sorted teosinte objects
-view(chr1_teosinte)
-chr2_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 2)
-chr3_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 3)
-chr4_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 4)
-chr5_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 5)
-chr6_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 6)
-chr7_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 7)
-chr8_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 8)
-chr9_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 9)
-chr10_teosinte <- sort_teosinte %>%
-  filter(Chromosome == 10)
-view(chr10_teosinte)
-write.table(chr1_maize, file = "chr1_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates file for maize
-view(chr1_maize.txt)
-write.table(chr2_maize, file = "chr2_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(chr1_maize, file = "chr1_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates .txt file for maize chromosome 1
+write.table(chr2_maize, file = "chr2_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #Repeat for all chromosomes
 write.table(chr3_maize, file = "chr3_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr4_maize, file = "chr4_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr5_maize, file = "chr5_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
@@ -119,7 +102,42 @@ write.table(chr7_maize, file = "chr7_maize.txt", sep = "\t", row.names = FALSE, 
 write.table(chr8_maize, file = "chr8_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr9_maize, file = "chr9_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr10_maize, file = "chr10_maize.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
-write.table(chr1_teosinte, file = "chr1_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates files for teosinte
+
+## Sort Joined Teosinte
+sort_teosinte <- join_teosinte %>%
+  arrange(as.numeric(Chromosome), as.numeric(Position)) #arrange teosinte object by chromosome and position
+view(sort_teosinte) #Check work
+chr1_teosinte <- sort_teosinte %>%  #create sorted teosinte objects
+  filter(Chromosome == 1) #Filter by chromosome 1
+view(chr1_teosinte) #Check work
+chr2_teosinte <- sort_teosinte %>% #Repeat for all chromosomes
+  filter(Chromosome == 2)
+view(chr2_teosinte)
+chr3_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 3)
+view(chr3_teosinte)
+chr4_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 4)
+view(chr4_teosinte)
+chr5_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 5)
+view(chr5_teosinte)
+chr6_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 6)
+view(chr6_teosinte)
+chr7_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 7)
+view(chr7_teosinte)
+chr8_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 8)
+view(chr8_teosinte)
+chr9_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 9)
+view(chr9_teosinte)
+chr10_teosinte <- sort_teosinte %>%
+  filter(Chromosome == 10)
+view(chr10_teosinte)
+write.table(chr1_teosinte, file = "chr1_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates .txt files for teosinte
 write.table(chr2_teosinte, file = "chr2_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr3_teosinte, file = "chr3_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr4_teosinte, file = "chr4_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
@@ -129,35 +147,45 @@ write.table(chr7_teosinte, file = "chr7_teosinte.txt", sep = "\t", row.names = F
 write.table(chr8_teosinte, file = "chr8_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr9_teosinte, file = "chr9_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr10_teosinte, file = "chr10_teosinte.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
-sort_desc_maize <- join_maize %>%
+
+## Maize in descending order with ? replaced with dash
+sort_desc_maize <- join_maize %>% #Create object for sorted descending maize
   arrange(as.numeric(Chromosome), desc(as.numeric(Position))) #sort maize in descending position
-view(sort_desc_maize)
+view(sort_desc_maize) #Check work
 maize_dash <- data.frame(lapply(sort_desc_maize, gsub, pattern = "[?]", replacement = "-")) #replace ? within brackets because it is wildcard
-view(maize_dash)
-chr1_maize_dash <- maize_dash %>% #creates sorted maize objects with dash
-  filter(Chromosome == 1)
-view(chr1_maize_dash)
-chr2_maize_dash <- maize_dash %>%
+view(maize_dash) #Check work
+chr1_maize_dash <- maize_dash %>% #creates descending sorted maize object where ? is replaced with -
+  filter(Chromosome == 1) #Filter by chromosome 1
+view(chr1_maize_dash) #Check work
+chr2_maize_dash <- maize_dash %>% #Repeat for all chromosomes
   filter(Chromosome == 2)
+view(chr2_maize_dash)
 chr3_maize_dash <- maize_dash %>%
   filter(Chromosome == 3)
+view(chr3_maize_dash)
 chr4_maize_dash <- maize_dash %>%
   filter(Chromosome == 4)
+view(chr4_maize_dash)
 chr5_maize_dash <- maize_dash %>%
   filter(Chromosome == 5)
+view(chr5_maize_dash)
 chr6_maize_dash <- maize_dash %>%
   filter(Chromosome == 6)
+view(chr6_maize_dash)
 chr7_maize_dash <- maize_dash %>%
   filter(Chromosome == 7)
+view(chr7_maize_dash)
 chr8_maize_dash <- maize_dash %>%
   filter(Chromosome == 8)
+view(chr8_maize_dash)
 chr9_maize_dash <- maize_dash %>%
   filter(Chromosome == 9)
+view(chr9_maize_dash)
 chr10_maize_dash <- maize_dash %>%
   filter(Chromosome == 10)
 view(chr10_maize_dash)
-write.table(chr1_maize_dash, file = "chr1_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates file for maize dash
-write.table(chr2_maize_dash, file = "chr2_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(chr1_maize_dash, file = "chr1_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates .txt file for maize sorted in descending order with dash
+write.table(chr2_maize_dash, file = "chr2_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #Repeat for all chromosomes
 write.table(chr3_maize_dash, file = "chr3_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr4_maize_dash, file = "chr4_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr5_maize_dash, file = "chr5_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
@@ -166,35 +194,46 @@ write.table(chr7_maize_dash, file = "chr7_maize_dash.txt", sep = "\t", row.names
 write.table(chr8_maize_dash, file = "chr8_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr9_maize_dash, file = "chr9_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr10_maize_dash, file = "chr10_maize_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
-sort_desc_teosinte <- join_teosinte %>%
+
+
+## Teosinte in descending order with ? replaced with dash
+sort_desc_teosinte <- join_teosinte %>% #Create object for sorting teosinte in descending order
   arrange(as.numeric(Chromosome), desc(as.numeric(Position))) #sort teosinte in descending position
-view(sort_desc_teosinte)
+view(sort_desc_teosinte) #Check work
 teosinte_dash <- data.frame(lapply(sort_desc_teosinte, gsub, pattern = "[?]", replacement = "-")) #replace ? within brackets because it is wildcard
-view(teosinte_dash)
+view(teosinte_dash) #check work
 chr1_teosinte_dash <- teosinte_dash %>% #creates sorted teosinte objects with dash
-  filter(Chromosome == 1)
-view(chr1_teosinte_dash)
-chr2_teosinte_dash <- teosinte_dash %>% 
+  filter(Chromosome == 1) #Filter by chromosome 1
+view(chr1_teosinte_dash) #Check work
+chr2_teosinte_dash <- teosinte_dash %>%  #Repeat for all chromosomes
   filter(Chromosome == 2)
+view(chr2_teosinte_dash)
 chr3_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 3)
+view(chr3_teosinte_dash)
 chr4_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 4)
+view(chr4_teosinte_dash)
 chr5_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 5)
+view(chr5_teosinte_dash)
 chr6_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 6)
+view(chr6_teosinte_dash)
 chr7_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 7)
+view(chr7_teosinte_dash)
 chr8_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 8)
+view(chr8_teosinte_dash)
 chr9_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 9)
+view(chr9_teosinte_dash)
 chr10_teosinte_dash <- teosinte_dash %>% 
   filter(Chromosome == 10)
 view(chr10_teosinte_dash)
-write.table(chr1_teosinte_dash, file = "chr1_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates file for teosinte dash
-write.table(chr2_teosinte_dash, file = "chr2_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(chr1_teosinte_dash, file = "chr1_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #creates .txt file for descending teosinte with dash
+write.table(chr2_teosinte_dash, file = "chr2_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE) #Repeat for all chromosomes
 write.table(chr3_teosinte_dash, file = "chr3_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr4_teosinte_dash, file = "chr4_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr5_teosinte_dash, file = "chr5_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
@@ -203,17 +242,19 @@ write.table(chr7_teosinte_dash, file = "chr7_teosinte_dash.txt", sep = "\t", row
 write.table(chr8_teosinte_dash, file = "chr8_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr9_teosinte_dash, file = "chr9_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(chr10_teosinte_dash, file = "chr10_teosinte_dash.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+
+## Data Visualization
 library(ggplot2) #load visualization library
 install.packages("ggpubr") #Package for better visualization
-library(ggpubr)
-sort_maize_bin <- sort_maize %>%
-  filter(Position != "unknown") %>%
-  filter(Position != "multiple") %>%
-  mutate(position_binned = cut(as.numeric(Position), 10)) %>%
-  filter(Chromosome != "multiple") %>%
-  filter(Chromosome != "unknown") %>%
-  mutate(Chromosome = fct_relevel(Chromosome, c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")))
-view(sort_maize_bin)
+library(ggpubr) #load package
+sort_maize_bin <- sort_maize %>% #Create binned object for sorted maize
+  filter(Position != "unknown") %>% #Filter everything but unknown positions
+  filter(Position != "multiple") %>% #Filter everything but multiple positions
+  mutate(position_binned = cut(as.numeric(Position), 10)) %>% #Cut chromosomes into 10 bins and make own column
+  filter(Chromosome != "multiple") %>% #Filter everything but multiple chromosomes
+  filter(Chromosome != "unknown") %>% #Filter everything but unknown chromosomes
+  mutate(Chromosome = fct_relevel(Chromosome, c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))) #Use levels to put visuals in chromosome order
+view(sort_maize_bin) #Check work
 maize <- ggplot(data = sort_maize_bin) +
   geom_bar(mapping = aes(x = position_binned, fill = Chromosome)) +
   xlab("Position(basepairs)") +
